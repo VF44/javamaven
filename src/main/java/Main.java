50658c5775840db1feef7b1794fc544fb9e9099d
vera <vera.filippova.79@mail.ru>
2024-05-17 00:15:42 +0300

    Javamaven added

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        long amount = 5000;
+        boolean registered = true;
+        BonusService service  = new BonusService();
+        long bonus = service.calculate(amount, registered);
+        System.out.println(bonus);
+
+
+    }
+}
+
