e0332b8b771fdb6a2a3f0fba094e45209f86735c
vera <vera.filippova.79@mail.ru>
2024-06-06 23:02:59 +0300

    Javamaven added

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -41,7 +41,7 @@ public class BonusServiceTest {
     void shouldCalculateForUnRegisteredAndOverLimit() {
         BonusService service = new BonusService();
         long amount = 1_000_000;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
         long actual = service.calculate(amount, registered);
 
